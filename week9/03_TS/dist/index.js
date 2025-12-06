f03039db13b11e80d889a2790f4125a879aeb112
Mayur Badgujar <136896698+mayurbadgujar03@users.noreply.github.com>
2025-12-06 09:43:50 +0530

    How to setup Typescript in project

---
+++ week9/03_TS/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `Helo ${person}, welcome to Chaicode`;
+}
+const username = "chai aur typescript";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ week9/03_TS/src/index.ts
@@ -0,0 +1,6 @@
+function greet(person: string): string {
+    return `Helo ${person}, welcome to Chaicode`
+}
+
+const username: string = "chai aur typescript";
+console.log(greet(username));
